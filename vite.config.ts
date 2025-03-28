5b7274a30dc398b8019010032db4b0291d42d4e4
Bhupendra <bpmaurya24th@gmail.com>
2025-03-28 16:40:46 +0530

    issue res on gh=pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Movie_Search"
+  base:"/Movie_Search/",
 })
